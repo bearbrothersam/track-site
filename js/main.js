7b7607b3c203b9935c90097bd5e1ddb18ab7703a
brother bear <LrdmDe.3005@gmail.com>
2024-04-21 23:52:45 +0900

    4.21編集中

---
+++ js/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// メインビジュアルスライド
+$(function() {
+	$('.main_image img:nth-child(n+2)').hide();
+	  setInterval(function() {
+		$(".main_image img:first-child").fadeOut(2000);
+		$(".main_image img:nth-child(2)").fadeIn(2000);
+		$(".main_image img:first-child").appendTo(".main_image");
+	},3000);
+});
+
+// ページトップへスクロール
+$('#page_top_link').click(function(){
+  $('body,html').animate({
+    scrollTop:0
+  },700);
+  return false;
+});
+
+// モーダルウインド
+$(function(){
+	let open = $('.modal-open'),
+		  close = $('.modal-close'),
+		  container = $('.modal-container');
+
+	open.on('click',function(){
+		container.addClass('active');
+		return false;
+	});
+
+	close.on('click',function(){
+		container.removeClass('active');
+	});
+});
+
+// ハンバーガーメニュー
+$('.openbtn').click(function () {
+	$(this).toggleClass('active');
+	$('.header__nav__sp').toggleClass('is-active');
+	$("body").toggleClass('noscroll');
+});
